f8bc27fbbd26d0247ac03662e810620353c5866f
sangjuneeeee <wns1169@gmail.com>
2023-12-02 21:50:44 +0900

    게시글이 없을 경우 표시할 라벨 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -18,6 +18,13 @@ const PostWrapper = styled.div`
   max-width: 1200px; /* 컨텐츠의 최대 너비를 설정합니다. 필요에 따라 조정 가능합니다. */
   margin: 0 auto; /* 중앙 정렬을 위해 자동 마진을 사용합니다. */
 `;
+const NothingLabel = styled.label`
+  position: absolute;
+  font-size: 20px;
+  transform: translateX(360%);
+  margin-top: 200px;
+`;
+
 
 const Main = () => {
     const [posts, setPosts] = useState([]);
@@ -26,63 +33,67 @@ const Main = () => {
 
 
         // 더미 데이터
-        const data = [
-                {
-                    memberEmail: "wns2349@naver.co",
-                    boardIndex: 1,
-                    boardTitle: "post 1",
-                    boardPrice: "1000",
-                    boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScY1JCmDBElwyYIfoEUdJSms3eIZqG8b7-Ig&usqp=CAU"
-            },{
-                memberEmail: "wns2349@na",
-                boardIndex: 2,
-                boardTitle: "post 2",
-                boardPrice: "10000",
-                boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxMMOlhiqWEJVNcSFzBpSfeYt2VQKeBImzg&usqp=CAU"
-            },
-            {
-                memberEmail: "wns2349@na",
-                boardIndex: 3,
-                boardTitle: "post 3",
-                boardPrice: "100",
-                boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxMMOlhiqWEJVNcSFzBpSfeYt2VQKeBImzg&usqp=CAU"
-            },
-            {
-                memberEmail: "wns2349@na",
-                boardIndex: 4,
-                boardTitle: "post 4",
-                boardPrice: "1000000",
-                boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxMMOlhiqWEJVNcSFzBpSfeYt2VQKeBImzg&usqp=CAU"
-            },
-            {
-                memberEmail: "wns2349@na",
-                boardIndex: 5,
-                boardTitle: "post 5",
-                boardPrice: "100000000",
-                boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxMMOlhiqWEJVNcSFzBpSfeYt2VQKeBImzg&usqp=CAU"
-            }
-        ];
-        setPosts(data);
+        // const data = [
+        //         {
+        //             memberEmail: "wns2349@naver.co",
+        //             boardIndex: 1,
+        //             boardTitle: "post 1",
+        //             boardPrice: "1000",
+        //             boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScY1JCmDBElwyYIfoEUdJSms3eIZqG8b7-Ig&usqp=CAU"
+        //     },{
+        //         memberEmail: "wns2349@na",
+        //         boardIndex: 2,
+        //         boardTitle: "post 2",
+        //         boardPrice: "10000",
+        //         boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxMMOlhiqWEJVNcSFzBpSfeYt2VQKeBImzg&usqp=CAU"
+        //     },
+        //     {
+        //         memberEmail: "wns2349@na",
+        //         boardIndex: 3,
+        //         boardTitle: "post 3",
+        //         boardPrice: "100",
+        //         boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxMMOlhiqWEJVNcSFzBpSfeYt2VQKeBImzg&usqp=CAU"
+        //     },
+        //     {
+        //         memberEmail: "wns2349@na",
+        //         boardIndex: 4,
+        //         boardTitle: "post 4",
+        //         boardPrice: "1000000",
+        //         boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxMMOlhiqWEJVNcSFzBpSfeYt2VQKeBImzg&usqp=CAU"
+        //     },
+        //     {
+        //         memberEmail: "wns2349@na",
+        //         boardIndex: 5,
+        //         boardTitle: "post 5",
+        //         boardPrice: "100000000",
+        //         boardFileIndex: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxMMOlhiqWEJVNcSFzBpSfeYt2VQKeBImzg&usqp=CAU"
+        //     }
+        // ];
+        // setPosts(data);
 
         //백엔드 연결
-        // const fetchPosts = async () => {
-        //     try {
-        //         const data = await getPostList();
-        //         setPosts(data);
-        //     } catch (error) {
-        //         console.error('게시글 불러오기 실패:', error);
-        //     }
-        // };
-        // fetchPosts();
+        const fetchPosts = async () => {
+            try {
+                const data = await getPostList();
+                setPosts(data);
+            } catch (error) {
+                console.error('게시글 불러오기 실패:', error);
+            }
+        };
+        fetchPosts();
     }, []);
 
     return (
 
         <Wrapper>
             <PostWrapper>
-                {posts.map(post => (
-                    <PostComponent key={post.boardIndex} {...post} />
-                ))}
+                {posts !== undefined ? (
+                    posts.map(post => (
+                        <PostComponent key={post.boardIndex} {...post} />
+                    ))
+                ) : (
+                    <NothingLabel>게시물이 없습니다.</NothingLabel>
+                )}
             </PostWrapper>
         </Wrapper>
 
