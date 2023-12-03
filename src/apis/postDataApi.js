9ed8adc5090034abe2d55c8f44456517972f580a
sangjuneeeee <wns1169@gmail.com>
2023-12-04 00:21:26 +0900

    hotFix

--- src/apis/postDataApi.js
+++ src/apis/postDataApi.js
@@ -21,11 +21,11 @@ export const postLogin = async (email, password) => {
 
 // 예약하기 api
 export const postDate = async (reservationData, postId, setOpen) => {
+    setOpen(false); // 예약창 닫기, 이 함수도 파라미터로 전달됩니다.
         return await instance.post('/reservation', {
             boardIndex: postId, // 게시글 ID는 함수의 파라미터로 전달됩니다.
             ...reservationData
         });
-        setOpen(false); // 예약창 닫기, 이 함수도 파라미터로 전달됩니다.
 };
 
 
@@ -34,6 +34,5 @@ export const postDate = async (reservationData, postId, setOpen) => {
 // 멀티파트/폼데이터 형식으로 보냄, 서버에서도 관리가 수월해짐.
 export const postUpload = async (formData) => {
         return await instance.post('/upload', formData, {headers: {"Contents-Type":"multipart/form-data"}});
-        console.log("formData : ", formData);
 };
 
