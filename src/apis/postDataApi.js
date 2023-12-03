577a09e310c47313de5e490a4e137bc3c574de52
sangjuneeeee <wns1169@gmail.com>
2023-12-04 00:18:32 +0900

    폼 데이터 헤더 설정

--- src/apis/postDataApi.js
+++ src/apis/postDataApi.js
@@ -33,7 +33,7 @@ export const postDate = async (reservationData, postId, setOpen) => {
 // 업로드 api
 // 멀티파트/폼데이터 형식으로 보냄, 서버에서도 관리가 수월해짐.
 export const postUpload = async (formData) => {
-        return await instance.post('/upload', formData);
+        return await instance.post('/upload', formData, {headers: {"Contents-Type":"multipart/form-data"}});
         console.log("formData : ", formData);
 };
 
